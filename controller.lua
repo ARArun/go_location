a115345074499e353d80e0f290c7461358b24e1e
Arun .A.R <arunnadar1994@gmail.com>
2017-08-22 15:44:53 +0530

    almost all states done

--- controller.lua
+++ controller.lua
@@ -4,6 +4,7 @@ lock = "locking"
 pullback = "pullback"
 function init()
     state = free
+    robot.turret.set_position_control_mode()
 end
 
 --need to lock turret
@@ -12,11 +13,11 @@ function step()
     if state == "free" then
         roam()
     elseif state == "contact" then
-        stop()
+        ang = stop()
     elseif state == "locking" then
-        turn_turret()
+        turn_turret(ang)
     elseif state == "pullback" then
-        return()
+        pull()
     end
 end
 
@@ -38,3 +39,39 @@ function roam()
         state = contact
     else
         robot.wheels.set_velocity(10,10)
+    end
+end
+
+--contact
+--here we stop when any part of robot
+--contacts the obstacle
+function stop()
+    sensor_no=0
+    sensor_angle=0
+    for i = 1,24 do
+        if robot.proximity[i].value == 1 then
+            robot.wheels.set_velocity(0,0)
+            state = lock
+            sensor_no = i
+            sensor_angle = robot.proximity[i].angle
+        end
+    end
+    return sensor_angle
+end
+
+--turn_turret
+--we turn the turret to the angle we need
+-- we pullback
+-- what if another robot comes near and then this state has arrived
+function turn_turret(ang)
+    robot.wheels.set_velocity(0,0)
+    robot.set_rotation(ang)
+    robot.gripper.lock_positive()
+    state = pullback
+end
+
+
+--function pull
+--going back to original position
+function pull ()
+    if math.abs(robot.positioning
