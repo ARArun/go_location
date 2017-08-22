cee4303615fba10250379fe797de690305d16176
Arun .A.R <arunnadar1994@gmail.com>
2017-08-22 14:06:54 +0530

    resetting

--- controller.lua
+++ controller.lua
@@ -7,16 +7,16 @@ end
 --why to lock turret
 function step()
     log("orientation = "..robot.positioning.orientation.angle)
-    if robot.proximity[12].value==1 then
+    if robot.proximity[1].value==1 then
         lock=1
         robot.gripper.lock_positive()
     end
     if robot.positioning.position.x >= 0 and lock ==1 then
         robot.set_velocity(0,0)
     elseif lock==1 then
-        robot.wheels.set_velocity(10,10)
-    else
         robot.wheels.set_velocity(-10,-10)
+    else
+        robot.wheels.set_velocity(10,10)
     end
 end
 
